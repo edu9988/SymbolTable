07a46ca01c6c5cb65e45fc17d8339d087f900217
edu9988 <edu_9988@hotmail.com>
2024-07-23 11:21:37 -0300

    WIP: working on 2-3 three insertion
    Auxiliary function downward() possibly finished. upward() still
    missing.

--- java/Arvore23.java
+++ java/Arvore23.java
@@ -7,6 +7,9 @@ public class Arvore23 implements SymbolTable{
 	readFile( filename );
     }
 
+    private void upward( TwoThreeCell parent , TwoThreeCell flyingNode ){
+    }
+
     private void downward( TwoThreeCell tree , String key ){
 	int aux;
 	aux = tree.p1.getKey().compareToIgnoreCase( key );
@@ -17,8 +20,21 @@ public class Arvore23 implements SymbolTable{
 		    if( tree.right == null ){
 			TwoThreeCell flyingTree = new TwoThreeCell();
 			flyingTree.threeNode = false;
-			flyingTree.p1.setKey( key );
-			upward( tree , flyingTree );
+			flyingTree.p1 = new Pair();
+			flyingTree.p1.setKey( tree.p2.getKey() );
+			flyingTree.p1.setVal( tree.p2.getVal() );
+			tree.p2 = null;
+			tree.threeNode = false;
+			flyingTree.parent = tree.parent;
+			tree.parent = flyingTree;
+			flyingTree.left = tree;
+			flyingTree.right = new TwoThreeCell();
+			flyingTree.right.threeNode = false;
+			flyingTree.right.parent = flyingTree;
+			flyingTree.right.p1 = new Pair();
+			flyingTree.right.p1.setKey( key );
+			flyingTree.right.p1.setVal( 1 );
+			upward( flyingTree.parent , flyingTree );
 		    }
 		    else
 			downward( tree.right , key );
@@ -28,29 +44,63 @@ public class Arvore23 implements SymbolTable{
 		}
 		else{ /* aux > 0 */
 		    if( tree.middle == null ){
-			upward( tree , key );
+			TwoThreeCell flyingNode = new TwoThreeCell();
+			flyingNode.threeNode = false;
+			flyingNode.p1 = new Pair();
+			flyingNode.p1.setKey( key );
+			flyingNode.p1.setVal( 1 );
+			flyingNode.left = tree;
+			flyingNode.parent = tree.parent;
+			tree.parent = flyingNode;
+			flyingNode.right = new TwoThreeCell();
+			flyingNode.right.parent = flyingNode;
+			flyingNode.right.threeNode = false;
+			flyingNode.right.p1 = new Pair();
+			flyingNode.right.p1.setKey( tree.p2.getKey() );
+			flyingNode.right.p1.setVal( tree.p2.getVal() );
+			tree.threeNode = false;
+			tree.p2 = null;
+			upward( flyingNode.parent , flyingNode );
 		    }
 		    else
 			downward( tree.middle , key );
 		}
 	    }
 	    else{ /* not threeNode */
-		if( tree.right == null )
+		if( tree.right == null ){
+		    tree.threeNode = true;
+		    tree.p2 = new Pair();
 		    tree.p2.setKey( key );
+		    tree.p2.setVal( 1 );
+		}
 		else
 		    downward( tree.right , key );
 	    }
 	}
-	else if( aux == 0 ){
+	else if( aux == 0 )
 	    tree.p1.valInc();
-	}
 	else{ /* aux > 0 */
 	    if( tree.left == null ){
 		if( tree.threeNode ){
 		    TwoThreeCell flyingNode = new TwoThreeCell();
 		    flyingNode.threeNode = false;
+		    flyingNode.p1 = new Pair();
 		    flyingNode.p1.setKey( tree.p1.getKey() );
 		    flyingNode.p1.setVal( tree.p1.getVal() );
+		    flyingNode.right = tree;
+		    flyingNode.parent = tree.parent;
+		    tree.parent = flyingNode;
+		    tree.p1.setKey( tree.p2.getKey() );
+		    tree.p1.setVal( tree.p2.getVal() );
+		    tree.threeNode = false;
+		    tree.p2 = null;
+		    flyingNode.left = new TwoThreeCell();
+		    flyingNode.left.parent = flyingNode;
+		    flyingNode.left.threeNode = false;
+		    flyingNode.left.p1 = new Pair();
+		    flyingNode.left.p1.setKey( key );
+		    flyingNode.left.p1.setVal( 1 );
+		    upward( flyingNode.parent , flyingNode );
 		}
 		else{ /* not threeNode */
 		    tree.p2 = new Pair();
@@ -72,7 +122,7 @@ public class Arvore23 implements SymbolTable{
 	    root.p1.setVal( 1 );
 	}
 	else
-	    downward( root, key );
+	    downward( root , key );
     }
 
     public int retrieve ( String key ){
